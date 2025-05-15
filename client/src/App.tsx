152de747a674161d9d8fdb8380e65df539d90a7e
spjegan87 <spjegan87@gmail.com>
2025-05-15 13:07:36 +0000

    Restored to '3c45be20a099e9c5f1c60e815fd7d8bec05d58e7'
    Replit-Restored-To: 3c45be20a099e9c5f1c60e815fd7d8bec05d58e7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Switch, Route } from "wouter";
-import "./i18n";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
